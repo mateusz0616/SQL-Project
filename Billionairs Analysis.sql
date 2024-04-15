4fc9a578b6baa6201001089d0799cf480f6511e1
mateuszEX <120175731+mateusz0616@users.noreply.github.com>
2024-04-15 17:24:17 +0200

    Update Billionairs Analysis.sql
    change  of comments

--- Billionairs Analysis.sql
+++ Billionairs Analysis.sql
@@ -1,29 +1,30 @@
 ——————————————————————————————————————————————
 
-*\table Billionairs*/
+--table Billionairs
 
 select *
 from Public."Billionairs"
 
-*\count of rows in table*/
+--count of rows in table
+	
 select count(*)
 from Public."Billionairs"
 
 
-\*1.Which billionaires has the highest wealth in the dataset?*/
+--1.Which billionaires has the highest wealth in the dataset?
 		
 select  CONCAT(first_name, ' ',last_name)
 from Public."Billionairs"
 where wealth = (select max(wealth) from Public."Billionairs")			
 
 
-\*2.What is the average age of billionaires in the dataset?*/
+--2.What is the average age of billionaires in the dataset?
 
 
 select  round(avg(age),2)
 from Public."Billionairs"
 
-\*3.How many billionaires live in each country?*/
+--3.How many billionaires live in each country?
 
 select  citizenship,count(distinct concat(first_name, ' ', last_name)) as cnt
 from Public."Billionairs"
@@ -31,7 +32,7 @@ group by citizenship
 order by cnt desc
 
 
-\*4.find out the name of  third billionaire the highest wealth in dataset*/
+--4.find out the name of  third billionaire the highest wealth in dataset
 
 
 select CONCAT(first_name, ' ',last_name)
@@ -40,7 +41,7 @@ order by wealth desc
 limit 1 offset 2
 
 
-\*5.What is the total wealth of billionaires in each industry?*/
+--5.What is the total wealth of billionaires in each industry?
 
 
 select industry,sum(wealth) as total
@@ -48,7 +49,7 @@ from Public."Billionairs"
 group by 1
 
 
-\*6.Who is the youngest individual in the dataset?*/
+--6.Who is the youngest individual in the dataset?
 
 
 select CONCAT(first_name, ' ',last_name),birth_date,citizenship
@@ -57,7 +58,7 @@ where age  = (select min(age) from Public."Billionairs" )
 
 
 
-\*7.Top 10 country which has the most individuals represented in the dataset?*/
+--7.Top 10 country which has the most individuals represented in the dataset?
 
 
 with a as (select citizenship,dense_rank() over(order by count(full_name) desc) as rnk,count(full_name) as cnt
@@ -71,7 +72,7 @@ from a
 where rnk <= 10
 
 
-\*8.What is the average wealth of individuals in each country?*/
+--8.What is the average wealth of individuals in each country?
 
 
 select citizenship,round(avg(age),4)
@@ -79,7 +80,7 @@ from Public."Billionairs"
 group by citizenship
 
 
-\*9.How many individuals belong to each industry?*/
+--9.How many individuals belong to each industry?
 
 
 select industry,count(*)
@@ -88,7 +89,7 @@ group by 1
 order by 2 desc
 
 
-\*10.Who is the oldest individual in the dataset?*/
+--10.Who is the oldest individual in the dataset?
 
 
 select  CONCAT(first_name, ' ',last_name),age
@@ -96,7 +97,7 @@ from Public."Billionairs"
 where age = (select max(age) from Public."Billionairs")	
 
 
-\*11.How many individuals reside in each city?*/
+--11.How many individuals reside in each city?
 
 
 select city_of_residence,count(*)
@@ -105,7 +106,7 @@ group by 1
 order by 2 desc
 
 
-\*12.What is the most common industry among individuals in a specific country?*/
+--12.What is the most common industry among individuals in a specific country?
 
 
 with a as(select citizenship,industry,dense_rank() over(partition by citizenship order by count(*) desc) as rnk
@@ -119,15 +120,14 @@ from a
 where rnk = 1
 
 
-\*13.What is the percentage of wealth owned by individuals under the age of 40?*/
+--13.What is the percentage of wealth owned by individuals under the age of 40?
 
 select wealth_under_40 / total_wealth * 100 AS percentage_wealth_under_40
 from (select sum(case when age <40 then wealth else 0 end) as wealth_under_40,sum(wealth) as total_wealth
 	from Public."Billionairs") as t
 
 
-\*14.How many individuals have a wealth above the median wealth of the dataset?*/
-
+--14.How many individuals have a wealth above the median wealth of the dataset?
 
 WITH a AS (SELECT *,ROW_NUMBER() OVER (ORDER BY wealth ASC) AS id_row
     FROM Public."Billionairs")
@@ -137,7 +137,7 @@ FROM a
 WHERE id_row > (SELECT (MAX(id_row) + 1) / 2 FROM a);
 
 
-\*15.For each city, what is the count of male and female individuals?find out  city with most male and female individuals */
+--15.For each city, what is the count of male and female individuals?find out  city with most male and female individuals?
 
 
 SELECT city_of_residence,
@@ -147,7 +147,7 @@ group by 1
 order by 2 desc ,3 desc
 
 
-\*16.Which industry has the highest average wealth per individual, and what is the average age of individuals in that industry?*/
+--16.Which industry has the highest average wealth per individual, and what is the average age of individuals in that industry?
 
 
 select industry,sum(wealth)/count(*) as avg_wealth,round(avg(age),0)
@@ -157,7 +157,7 @@ order by 2 desc
 limit 1
 
 
-\*17.  Find the top 5% of wealthy individuals and their respective countries of residence.*/
+--17.  Find the top 5% of wealthy individuals and their respective countries of residence?
 
 select *
 FROM Public."Billionairs"
@@ -165,7 +165,7 @@ order by wealth desc
 limit(SELECT COUNT(*) * 0.05 FROM Public."Billionairs")
 
 
-\*18.What is the distribution of wealth and the number of people in different industries by age group?*/
+--18.What is the distribution of wealth and the number of people in different industries by age group?
 
 
 SELECT
@@ -188,7 +188,7 @@ ORDER BY
     age_group;
 
 
-\*19.   What is the average wealth difference between the top 10% and bottom 10% of individuals based on their ranks?*/
+--19.   What is the average wealth difference between the top 10% and bottom 10% of individuals based on their ranks?
 
 
 with a as(select *
@@ -203,20 +203,20 @@ avg(case when percentile_rank <=0.1 then percentile_rank end) as bottom10,
 (avg(case when percentile_rank >=0.9 then percentile_rank end)-avg(case when percentile_rank <=0.1 then percentile_rank end)) as difference
 from a
 
-20.Find out how many individuals was born on June*/
+--20.Find out how many individuals was born on June
 
 SELECT count(*)
 FROM Public."Billionairs"
 where EXTRACT(MONTH FROM birth_date) = 06
 
 
-\*21.find out individuals  that have other citizenship then  their country residence*/
+--21.find out individuals  that have other citizenship then  their country residence
 
 SELECT count(*)/sum(case when  citizenship <> country_of_residence then 1 else 0 end )
 FROM Public."Billionairs"
 
 
-\*22.What is the number of richest people in the table, grouped by day of the week*/
+--22.What is the number of richest people in the table, grouped by day of the week
 
 select day_of_week,count(concat(first_name,' ',last_name))
 from (SELECT *,TO_CHAR(birth_date, 'Day') AS day_of_week
